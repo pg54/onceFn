ef9fe34f008d679a50b9c2bd2439f35caea61b87
pg54 <pangang0817@icloud.com>
2018-01-12 18:11:01 +0800

    ok

---
+++ async-validator/index.vue
@@ -0,0 +1,50 @@
+// https://juejin.im/post/59f09e135188255f9b339817
+<template>
+    <form class="v-form">
+        <slot></slot>
+    </form>
+</template>
+<script>
+    import  AsyncValidator  from 'async-validator'
+    export default {
+        name: 'v-form',
+        componentName: 'vForm',
+        data () {
+            return {
+                fields: [],
+                formError: {}
+            }
+        },
+        computed: {
+            formRules () {
+                const descriptor = {}
+                this.fields.forEach((prop) => {
+                    if (!Array.isArray(this.rules[prop])) {
+                        console.warn(`prop 为 ${prop} 的 FormItem 校验规则不存在或者其值不是数组`)
+                        descriptor[prop] = [{required: true}]
+                        return 
+                    }
+                    descriptor[prop] = this.rules[prop]
+                })
+                return descriptor
+            },
+            formValues () {
+                return this.fields.reduce((data, {prop}) => {
+                    data[prop] = this.model[prop]
+                    return data
+                },{})
+            }
+        },
+        methods: {
+            validate (callback) {
+                const validtor = new AsyncValidator(this.formValues)
+                validtor.validtor(this.formValues, (errors) => {
+                    let formError = {}
+                    if () {
+                        
+                    }
+                })
+            }
+        }
+    }
+</script>
