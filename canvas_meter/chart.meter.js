04fada4a3f44093e4fd89f56ddd797c15f862294
pan gang <1260424774@qq.com>
2018-05-04 20:55:01 +0800

    o
    o

--- canvas_meter/chart.meter.js
+++ canvas_meter/chart.meter.js
@@ -1,10 +1,4 @@
-/**
- * 
- * @authors 王昱森
- * @date    2015-08-14 11:31:56
- * @version 1.0.3
- */
-var Meter = (function () {
+var Meter = (function() {
 
     var options = {
 
@@ -13,9 +7,9 @@ var Meter = (function () {
             eAngle: 2.07,
             area: {
                 radius: 30,
-                colors: { 
-                    '0': '#1266BC', 
-                    '0.15': '#67C6F2', 
+                colors: {
+                    '0': '#1266BC',
+                    '0.15': '#67C6F2',
                     '0.27': '#45F5E6',
                     '0.75': '#FFDE00',
                     '0.93': '#F5694B',
@@ -32,7 +26,7 @@ var Meter = (function () {
                 arrow: {
                     height: 15,
                     radius: 4
-                } 
+                }
             },
             value: {
                 margin: -50,
@@ -63,8 +57,8 @@ var Meter = (function () {
         }
     };
 
-    var element, 
-        context, 
+    var element,
+        context,
         styles,
         sAngle,
         eAngle,
@@ -76,9 +70,9 @@ var Meter = (function () {
         labelStyle,
         innerStyle;
 
-    var extend = function(obj1, obj2){
-        for(var k in obj2) {
-            if(obj1.hasOwnProperty(k) && typeof obj1[k] == 'object') {
+    var extend = function(obj1, obj2) {
+        for (var k in obj2) {
+            if (obj1.hasOwnProperty(k) && typeof obj1[k] == 'object') {
                 extend(obj1[k], obj2[k]);
             } else {
                 obj1[k] = obj2[k];
@@ -86,7 +80,7 @@ var Meter = (function () {
         }
     }
 
-    var calcLocation = function(r, end){
+    var calcLocation = function(r, end) {
 
         return {
             x: options.centerPoint.x + r * Math.cos(Math.PI * end),
@@ -94,22 +88,22 @@ var Meter = (function () {
         };
     }
 
-    var calcValueRange = function(value){
+    var calcValueRange = function(value) {
         var data = options.data.area,
             index = data.length - 1;
 
         for (var i = index; i >= 0; i--) {
-            if(value >= data[i].min && value < data[i].max){
+            if (value >= data[i].min && value < data[i].max) {
                 index = i;
             }
         };
-        var r = (eAngle - sAngle)/data.length,
+        var r = (eAngle - sAngle) / data.length,
             s = r * index + sAngle,
             e = r * (index + 1) + sAngle,
             o = data[index];
 
         return {
-            range: (value - o.min)/(o.max - o.min) * (e - s) + s,
+            range: (value - o.min) / (o.max - o.min) * (e - s) + s,
             index: index
         };
     }
@@ -123,27 +117,27 @@ var Meter = (function () {
         context.beginPath();
         context.moveTo(x, y);
 
-        switch(flag){
-            case 1: 
+        switch (flag) {
+            case 1:
                 context.setLineDash && context.setLineDash([innerStyle.dashedWidth]);
             case 2:
-                context.arc(x, y, opts.r, Math.PI*s, Math.PI*e);
+                context.arc(x, y, opts.r, Math.PI * s, Math.PI * e);
                 context.closePath();
                 context.strokeStyle = opts.style;
                 context.stroke();
                 break;
             default:
-                context.arc(x, y, opts.r, Math.PI*s, Math.PI*e);
+                context.arc(x, y, opts.r, Math.PI * s, Math.PI * e);
                 context.closePath();
                 context.fillStyle = opts.style;
                 context.fill();
                 break;
-        }      
+        }
     }
 
-    var drawArea = function(){
-        var grad  = context.createLinearGradient(0, 0, options.radius*2, 0);
-        for(var k in areaStyle.colors) {
+    var drawArea = function() {
+        var grad = context.createLinearGradient(0, 0, options.radius * 2, 0);
+        for (var k in areaStyle.colors) {
             grad.addColorStop(k, areaStyle.colors[k]);
         }
 
@@ -160,7 +154,7 @@ var Meter = (function () {
         });
     }
 
-    var drawValueRange = function(valueRange){
+    var drawValueRange = function(valueRange) {
 
         var r = options.radius - areaStyle.radius;
 
@@ -184,16 +178,16 @@ var Meter = (function () {
         });
     }
 
-    var fillText = function(opts){
-        context.font = opts.font; 
+    var fillText = function(opts) {
+        context.font = opts.font;
         context.fillStyle = opts.color;
         context.textAlign = opts.align || 'center';
-        context.textBaseline = opts.vertical || 'middle';  
-        context.moveTo(opts.x, opts.y);  
-        context.fillText(opts.text, opts.x, opts.y); 
+        context.textBaseline = opts.vertical || 'middle';
+        context.moveTo(opts.x, opts.y);
+        context.fillText(opts.text, opts.x, opts.y);
     }
 
-    var drawInnerContent = function(valueRange, value){
+    var drawInnerContent = function(valueRange, value) {
         drawCircle({
             r: innerStyle.radius,
             start: sAngle,
@@ -207,7 +201,7 @@ var Meter = (function () {
         });
 
         var data = options.data;
-        
+
         fillText({
             font: valueStyle.font,
             color: valueStyle.color,
@@ -233,10 +227,10 @@ var Meter = (function () {
         });
     }
 
-    var drawArrow = function(valueRange){
+    var drawArrow = function(valueRange) {
         var r = options.radius - areaStyle.radius - labelStyle.radius,
             loc = calcLocation(r, valueRange.range),
-            x = loc.x - 1, 
+            x = loc.x - 1,
             y = loc.y + 0.5;
 
         drawCircle({
@@ -245,7 +239,7 @@ var Meter = (function () {
             r: rangeStyle.arrow.radius,
             style: rangeStyle.color
         });
-        
+
         var a = calcLocation(r - rangeStyle.arrow.height, valueRange.range),
             b = calcLocation(r, valueRange.range - 0.01),
             c = calcLocation(r, valueRange.range + 0.01);
@@ -276,58 +270,58 @@ var Meter = (function () {
         context.stroke();
     }
 
-    var drawTickMarks = function(){
+    var drawTickMarks = function() {
         var scaleLength = areaStyle.scaleLength,
             data = options.data.area,
             len = scaleLength * data.length,
-            range = (eAngle - sAngle)/len;
+            range = (eAngle - sAngle) / len;
 
-        for(var j = 1; j < len; j++){
+        for (var j = 1; j < len; j++) {
             drawLine({
                 start: calcLocation(options.radius, sAngle + range * j),
                 end: calcLocation(options.radius - areaStyle.radius, sAngle + range * j),
                 style: areaStyle.lineColor,
-                width: j % scaleLength == 0 ? areaStyle.lineWidth: areaStyle.scaleWidth
+                width: j % scaleLength == 0 ? areaStyle.lineWidth : areaStyle.scaleWidth
             });
         }
 
         var lblArr = [];
-        for(var i = 0; i < data.length; i++){
+        for (var i = 0; i < data.length; i++) {
             var o = data[i];
             // 如果不需兼容IE9以下则不用join
-            if(lblArr.join('').indexOf(o.min) == -1) {
+            if (lblArr.join('').indexOf(o.min) == -1) {
                 lblArr.push(o.min);
             }
             lblArr.push(o.text);
             lblArr.push(o.max);
 
         }
-        
-        var lblLen = lblArr.length - 1,
-            lblRange = (eAngle - sAngle)/lblLen,
-            lblOpt = labelStyle,
-            lblR = options.radius - areaStyle.radius - lblOpt.radius/2;
 
-        for(var k = 0; k <= lblLen; k++){
+        var lblLen = lblArr.length - 1,
+            lblRange = (eAngle - sAngle) / lblLen,
+            lblOpt = labelStyle,
+            lblR = options.radius - areaStyle.radius - lblOpt.radius / 2;
+
+        for (var k = 0; k <= lblLen; k++) {
             var loc = calcLocation(lblR, sAngle + lblRange * k);
             lblOpt.x = loc.x;
             lblOpt.y = loc.y;
             lblOpt.text = lblArr[k];
             fillText(lblOpt);
         }
-        
+
     }
 
     var drawing = function(w, h) {
         var value = options.data.value,
             valueTemp = options.data.area[0].min;
 
-        var timer = setInterval(function(){
+        var timer = setInterval(function() {
             context.clearRect(0, 0, w, h);
             context.fillStyle = "#fff";
             context.fillRect(0, 0, w, h);
 
-            valueTemp = valueTemp + 10 > value ? value: valueTemp + 10;
+            valueTemp = valueTemp + 10 > value ? value : valueTemp + 10;
             var valueRange = calcValueRange(valueTemp);
 
             drawArea();
@@ -336,7 +330,7 @@ var Meter = (function () {
             drawArrow(valueRange);
             drawTickMarks();
 
-            if(valueTemp === value) {
+            if (valueTemp === value) {
                 clearInterval(timer);
             }
         }, 10);
@@ -344,7 +338,7 @@ var Meter = (function () {
 
     var exports = {};
 
-    exports.setOptions = function(opts){
+    exports.setOptions = function(opts) {
         extend(options, opts);
 
         styles = options.styles;
@@ -363,7 +357,7 @@ var Meter = (function () {
         return exports;
     };
 
-    exports.init = function(){
+    exports.init = function() {
         drawing(element.offsetWidth, element.offsetHeight);
         return exports;
     }
